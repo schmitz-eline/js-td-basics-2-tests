146ec090d57a44a44f309c49ee9fb02dbe225578
schmitz-eline <eline.schmitz@student.hepl.be>
2025-10-03 18:42:25 +0200

    ch3 ex2

--- OCR-starter/chapitre3_exercice2.js
+++ OCR-starter/chapitre3_exercice2.js
@@ -11,3 +11,15 @@
 // Exemple : si la moyenne du candidat est de 11, le programme devrait afficher "Le candidat est reçu."
 
 // N'oubliez pas de tester votre programme avec différentes moyennes pour couvrir tous les cas de figure.
+
+const average = 11;
+
+if (average >= 0 && average < 10) {
+    console.log('Le candidat est recalé avec une moyenne de ' + average + '. Il faut obtenir 10 pour être reçu');
+} else if (average >= 10 && average <= 12) {
+    console.log('Le candidat est reçu avec une moyenne de ' + average);
+} else if (average > 12 && average <= 20) {
+    console.log('Le candidat est reçu avec mention avec une moyenne de ' + average);
+} else {
+    console.log('La moyenne doit être comprise entre 0 et 20');
+}

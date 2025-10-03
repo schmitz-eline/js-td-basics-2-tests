ca07b3f58964ad393cfac01bede1c766a4d15a94
schmitz-eline <eline.schmitz@student.hepl.be>
2025-10-03 18:27:47 +0200

    ch3 ex1

--- OCR-starter/chapitre3_exercice1.js
+++ OCR-starter/chapitre3_exercice1.js
@@ -17,3 +17,37 @@
 // Exemple : si le jour actuel est "mardi", le programme devrait afficher "Demain, nous serons mercredi."
 
 // N'oubliez pas de tester votre programme avec divers jours de la semaine.
+
+const weekDays = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche'];
+
+const currentIndex = new Date().getDay() - 1;
+// Pour obtenir l'indice du jour actuel. On fait -1, car les américains commencent par dimanche, or, on veut commencer par lundi (indice 0).
+
+const currentDay = weekDays[currentIndex];
+
+const nextIndex = (currentIndex + 1) % weekDays.length;
+// On fait %7 pour que quand on arrive à dimanche (indice 6, auquel on ajoute 1), on retourne à lundi (indice 0 = reste de la division de 7 par 7).
+
+const nextDay = weekDays[nextIndex];
+
+if (nextDay !== '') {
+    if (currentIndex < 0 || currentIndex > 6) {
+        console.log('Erreur : jour non reconnu !');
+    }
+
+    if (
+        currentDay !== weekDays[0] &&
+        currentDay !== weekDays[1] &&
+        currentDay !== weekDays[2] &&
+        currentDay !== weekDays[3] &&
+        currentDay !== weekDays[4] &&
+        currentDay !== weekDays[5] &&
+        currentDay !== weekDays[6] &&
+        typeof currentDay !== 'string'
+    ) {
+        console.log(currentDay + ' n’est pas un jour de la semaine');
+    } else {
+        console.log('Aujourd’hui, nous sommes ' + currentDay);
+        console.log('Demain, nous serons ' + nextDay);
+    }
+}

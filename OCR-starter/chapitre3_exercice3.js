ebb36b8cd8da7dfc376748a738298e1c6e7ccbc6
schmitz-eline <eline.schmitz@student.hepl.be>
2025-10-04 11:06:12 +0200

    ch3 ex3

--- OCR-starter/chapitre3_exercice3.js
+++ OCR-starter/chapitre3_exercice3.js
@@ -10,3 +10,23 @@
 // Si l'heure actuelle était égale à 23, réinitialisez l'heure à 0 (minuit).
 //     Sinon, incrémentez l'heure actuelle.
 // Affichez la nouvelle heure, les minutes et les secondes dans la console.
+
+let now = new Date();
+let hour = now.getHours();
+let minute = now.getMinutes();
+let second = now.getSeconds();
+
+second += 1;
+if (second === 60) {
+    second = 0;
+    minute += 1;
+    if (minute === 60) {
+        minute = 0;
+        hour += 1;
+        if (hour === 24) {
+            hour = 0;
+        }
+    }
+}
+
+console.log('Il est ' + hour + 'h, ' + minute + 'm et ' + second + 's');

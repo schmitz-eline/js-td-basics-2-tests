c0979b6c9fd6f351281a7d6a85cb6fe3de6c2e5d
schmitz-eline <eline.schmitz@student.hepl.be>
2025-10-05 18:48:22 +0200

    tests-p1

--- starter/tests-p1.js
+++ starter/tests-p1.js
@@ -11,3 +11,15 @@ sachant qu’on accorde 10% de ristourne si le client achète au moins 20 articl
 //    a) Si la condition est vraie, calculez la ristourne de 10% sur le montant total (0.1 * montant total) et soustrayez-la du montant total.
 //    b) Si la condition est fausse, laissez le montant total tel quel.
 // 4. Affichez le montant total de la facture dans la console.
+
+const nombreArticles = prompt('Entrez le nombre d’articles achetés par le client.');
+const prixUnitaire = prompt('Entrez le prix unitaire d’un article.');
+let montantTotal = nombreArticles * prixUnitaire;
+const ristourne = 0.1 * montantTotal;
+
+if (montantTotal >= 20) {
+    montantTotal = montantTotal - ristourne;
+    console.log('Vous obtenez une ristourne de 10%. La facture est de ' + Math.round(montantTotal * 100) / 100 + ' €.');
+} else {
+    console.log('La facture est de ' + Math.round(montantTotal * 100) / 100 + ' €.');
+}
